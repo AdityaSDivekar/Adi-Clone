859626df5ae3c7e05f4e38a900af33abdfd327fb
AdityaSDivekar <adityadivekar1712@gmail.com>
2023-02-03 23:27:01 +0530

    First Program

---
+++ Program1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+
+class Program1
+{
+    public static void main(String args[])
+    {
+        Scanner sobj=new Scanner(System.in);
+        System.out.println("Jay Ganesha....");
+    }
+}
