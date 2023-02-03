f3817e474642f90b68db6bc1b8936f8c5bbdf499
AdityaSDivekar <adityadivekar1712@gmail.com>
2023-02-03 23:30:02 +0530

    First Code Of  Engg.

---
+++ Program2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+
+class Program2
+{
+    public static void main(String args[])
+    {
+        Scanner sobj=new Scanner(System.in);
+        System.out.println("Jay Ganesha....");
+    }
+}
